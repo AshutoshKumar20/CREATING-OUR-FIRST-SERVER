60fcb9bc803f907c273443c557fddc6faf0ab50d
AshutoshKumar20 <ashutoshkumar44549@gmail.com>
2022-10-28 07:58:21 +0530

    Adding outes to server completed

--- server.js
+++ server.js
@@ -1,11 +1,62 @@
 const http = require('http');
-
 const port = 8081;
+
+const toDoList = ["Complete Node Byte", "Playing Cricket"];
 
 http
     .createServer((req, res) => {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.write("<h1>Hello, this is from my Server</h1>");
+        const { method, url } = req;
+
+        if (url === "/todos") {
+            if (method === "GET") {
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.write(toDoList.toString());
+            } else if (method === "POST") {
+                let body = '';
+                req.on("error", (err) => {
+                    console.error(err);
+                }).on("data", (chunk) => {
+                    body += chunk;
+                }).on("end", () => {
+                    body = JSON.parse(body);
+                    let newToDo = toDoList;
+                    newToDo.push(body.item);
+                    console.log(newToDo);
+                    res.writeHead(201);
+                });
+
+            } else if (method === "DELETE") {
+                let body = '';
+                req.on('error', (err) => {
+                    console.log(err);
+                }).on('data', (chunk) => {
+                    body += chunk;
+                }).on('end', () => {
+                    body = JSON.parse(body);
+                    let deleteThis = body.item;
+
+                    /* for (let i = 0; i < toDoList.length; i++) {
+                         if (toDoList[i] === deleteThis) {
+                             toDoList.splice(i, 1);
+                             break;
+                         }
+                      }*/
+
+                    toDoList.find((element, index) => {
+                        if (element === deleteThis) {
+                            toDoList.splice(index, 1);
+                        }
+                    });
+
+                    res.writeHead(204);
+                });
+            } else {
+                res.writeHead(501);
+            }
+        } else {
+            res.writeHead(404);
+        }
+
         res.end();
     })
     .listen(port, () => {
